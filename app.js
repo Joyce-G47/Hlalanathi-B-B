8485f6748b6fdbcb68705e35e1392b229a602d2e
Joyce-G47 <sibongilegumbi47@gmail.com>
2024-05-18 20:53:51 +0200

    Special Offer

--- app.js
+++ app.js
@@ -18,3 +18,41 @@ function showSlides() {
     setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
+// script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const offers = [
+      {
+        title: "50% Off Winter Special",
+        description: "Get 50% off on our exclusive summer collection. Limited time offer!",
+        image: "50%.jpg"
+      },
+      {
+        title: "Couples retreat",
+        description: "Buy one get one free on select items. Don't miss out!",
+        image: "Buyonegetone.jpg"
+      },
+      {
+        title: "Free Attraction Tour ",
+        description: "Enjoy free shipping on all orders over $50. Shop now!",
+        image: "30%.jpg"
+      }
+    ];
+  
+    const offersContainer = document.getElementById("offers-container");
+  
+    offers.forEach(offer => {
+      const offerElement = document.createElement("div");
+      offerElement.classList.add("offer");
+  
+      offerElement.innerHTML = `
+        <img src="${offer.image}" alt="${offer.title}">
+        <h3>${offer.title}</h3>
+        <p>${offer.description}</p>
+        <button>Learn More</button>
+      `;
+  
+      offersContainer.appendChild(offerElement);
+    });
+  });
+  
